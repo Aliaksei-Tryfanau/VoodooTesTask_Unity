076a013a54f84501c604abf526d7ca23552a4144
Aliaksei-Tryfanau <altrishka@gmail.com>
2022-10-09 15:53:23 -0400

    Game screen and script

---
+++ Assets/Scripts/UI/GameScreen.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameScreen : MonoBehaviour
+{
+    [SerializeField] private Button startButton;
+    [SerializeField] private Button randomizeButton;
+
+    private void Start()
+    {
+        startButton.onClick.AddListener(OnStartClick);
+        randomizeButton.onClick.AddListener(PawnsManager.Instance.RandomizePawns);
+    }
+
+    private void OnStartClick()
+    {
+        startButton.gameObject.SetActive(false);
+        randomizeButton.gameObject.SetActive(false);
+        PawnsManager.Instance.StartSimulation();
+    }
+}
